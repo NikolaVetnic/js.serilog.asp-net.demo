e5cf8ccb7a03780826a98c2b055652618eb955d6
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-08-20 21:45:17 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Structured Logging Using Serilog In ASP.NET Core 7.0
+
+Link: [https://www.c-sharpcorner.com/article/structured-logging-using-serilog-in-asp-net-core-7-0/](https://www.c-sharpcorner.com/article/structured-logging-using-serilog-in-asp-net-core-7-0/)
---
+++ SerilogApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SerilogApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+        _logger.LogInformation("Weather controller called ");
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        _logger.LogInformation("WeatherForecast GET method starting.");
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
